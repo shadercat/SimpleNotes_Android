e1e60eb109eec5ea8fb174ae87da1ad861716045
Thang Tran <tran.tkhanh@outlook.com>
2020-12-13 11:19:54 +0200

    Added context menu on long item click.

--- app/src/main/java/suan/chan/pzpi_17_8/MainActivity.java
+++ app/src/main/java/suan/chan/pzpi_17_8/MainActivity.java
@@ -1,5 +1,6 @@
 package suan.chan.pzpi_17_8;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,7 +8,9 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -61,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void itemOnLongClick(int itemIndex) {
-                Toast.makeText(getApplicationContext(), "Long click on " + notesList.get(itemIndex).getTitle(), Toast.LENGTH_SHORT).show();
+                initClickAlertDialog(itemIndex);
             }
         };
 
@@ -76,5 +79,40 @@ public class MainActivity extends AppCompatActivity {
         noteList.addItemDecoration(itemDecoration);
     }
 
+    private void initClickAlertDialog(long itemIndex){
 
+        final String[] options = {getString(R.string.edit), getString(R.string.delete)};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View inflater = this.getLayoutInflater().inflate(R.layout.dialog_note_context, null);
+        inflater.findViewById(R.id.dialog_context_delete_btn).setOnClickListener(clickAlertDialogResultHandler(itemIndex));
+        inflater.findViewById(R.id.dialog_context_edit_btn).setOnClickListener(clickAlertDialogResultHandler(itemIndex));
+        builder.setView(inflater)
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private View.OnClickListener clickAlertDialogResultHandler(final long itemIndex){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.dialog_context_edit_btn:
+                        Toast.makeText(getApplicationContext(), "Edit " + itemIndex, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.dialog_context_delete_btn:
+                        Toast.makeText(getApplicationContext(), "Delete " + itemIndex, Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+    }
 }
